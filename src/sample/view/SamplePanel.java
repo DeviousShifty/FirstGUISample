1321c54a76a5b2d6bfb3157fa0a41f4876a95128
Ethan M <guitarworthy365@gmail.com>
2018-11-27 08:57:58 -0700

    Button now when clicked says I clicked a button in the console.

--- src/sample/view/SamplePanel.java
+++ src/sample/view/SamplePanel.java
@@ -8,44 +8,40 @@ import java.awt.Color;
 
 import javax.swing.*;
 
-public class SamplePanel extends JPanel
-{
+public class SamplePanel extends JPanel {
 	private GUIController appController;
-	
+
 	private JButton colorButton;
 	private JLabel textLabel;
-	
-	public SamplePanel(GUIController appController)
-	{
+
+	public SamplePanel(GUIController appController) {
 		super();
-		
+
 		this.appController = appController;
-		
+
 		colorButton = new JButton("Click to change the color");
 		textLabel = new JLabel("This is a color app");
-		
+
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
-	private void setupPanel()
-	{
-	this.setBackground(Color.MAGENTA);
-	this.add(textLabel);
-	this.add(colorButton);
+
+	private void setupPanel() {
+		this.setBackground(Color.MAGENTA);
+		this.add(textLabel);
+		this.add(colorButton);
 	}
-	private void setupLayout()
-	{
-		
+
+	private void setupLayout() {
+
 	}
-	
-	private void setupListeners()
-	{
-		colorButton.addActionListener(new ActionListener() 
-		{
-			public void actionPerformed(ActionEvent mouseClick)
+
+	private void setupListeners() {
+		colorButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent mouseClick) 
 			{
-				
+				System.out.println("I clicked a button! :)");
 			}
 		});
 	}
