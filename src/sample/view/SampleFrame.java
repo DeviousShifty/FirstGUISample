39782c97ba59ea0c645d6dfec8a6e668f559ad66
Ethan M <guitarworthy365@gmail.com>
2018-11-27 08:15:20 -0700

    Finished SampleFrame class.

--- src/sample/view/SampleFrame.java
+++ src/sample/view/SampleFrame.java
@@ -1,7 +1,29 @@
-package sample.view;
+package sample.view; //Frame class always looks like this
+//IMPORTS
 import javax.swing.JFrame;
 import GUI.Controller.GUIController;
-public class SampleFrame 
-{
 
+public class SampleFrame extends JFrame
+{
+	private GUIController appController;
+	private SamplePanel appPanel;
+													//constructor initializes the data member
+	public SampleFrame(GUIController appController) //constructor in frame class takes the controller as a parameter
+	{
+		super(); //first line of constructor after every class that uses extends
+		
+		this.appController = appController; //different color for parameter and data member
+		this.appPanel = new SamplePanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame() 
+	{
+		this.setContentPane(appPanel);
+		this.setSize(1000, 500);
+		this.setTitle("Second GUI App");
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
