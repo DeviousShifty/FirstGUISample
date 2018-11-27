349ebe9e5d58a6e4a53eb364026ac94fd0f03998
Ethan M <guitarworthy365@gmail.com>
2018-11-27 08:27:12 -0700

    Built upon Gui controller.

--- src/GUI/Controller/GUIRunner.java
+++ src/GUI/Controller/GUIRunner.java
@@ -1,5 +1,10 @@
 package GUI.Controller;
 
-public class GUIRunner {
-
+public class GUIRunner 
+{
+	public static void main(String [] args)
+	{
+		GUIController app = new GUIController();
+		app.start();
+	}
 }
